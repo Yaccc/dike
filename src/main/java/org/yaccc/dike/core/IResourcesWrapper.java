99e097cf4ec5bb7dfcd8e2adc90070e74ec217d2
谢照东-代码投毒专家 <xiezhaodong@meituan.com>
2018-03-25 17:45:58 +0800

    init resoures wrapper

---
+++ src/main/java/org/yaccc/dike/core/IResourcesWrapper.java
@@ -0,0 +1,16 @@
+package org.yaccc.dike.core;
+
+import lombok.Data;
+
+/**
+ * Created by xiezhaodong  on 2018/3/25
+ * resources wrapper,
+ */
+@Data
+public abstract class IResourcesWrapper {
+    //
+    private String rName;
+    private String rType;
+
+
+}
